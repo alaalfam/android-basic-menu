0c02702aa90931b44d3866b6ae145e230ed9388e
Arman <alaalfam@gmail.com>
2019-07-24 22:21:54 +0430

    create menu with a subMenu

---
+++ app/src/main/java/com/snowman/menu/MainActivity.java
@@ -0,0 +1,34 @@
+package com.snowman.menu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        menu.add("MyItem1");
+//        menu.add("MyItem2");
+//        SubMenu subMenu = menu.addSubMenu("SubMenu");
+//        subMenu.add("setting");
+//        subMenu.add("advance");
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+        return super.onOptionsItemSelected(item);
+    }
+}
